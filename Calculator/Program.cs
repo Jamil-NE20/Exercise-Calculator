8efa7133728ba6a135c23021a7c6237c311a41d6
Md Jamil Uddin <azad1031@gmail.com>
2020-05-13 14:22:34 +0200

    Exercise Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.Write("Enter The FirstNumber: ");
+            int FirstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter The Operator: ");
+            char Operator = Convert.ToChar(Console.ReadLine());
+            Console.Write("Enter The SecondNumber: ");
+            int SecondNumber = Convert.ToInt32(Console.ReadLine());
+            if (Operator == '+')
+            { 
+                Console.WriteLine("Output:\n {0} + {1} = {2}", FirstNumber, SecondNumber, FirstNumber + SecondNumber);
+            }
+            else if (Operator == '-')
+            {
+                Console.WriteLine("Output:\n {0} - {1} = {2}", FirstNumber, SecondNumber, FirstNumber - SecondNumber);
+            }   
+            else if (Operator == '*')
+            {
+                Console.WriteLine("Output:\n {0} * {1} = {2}", FirstNumber, SecondNumber, FirstNumber * SecondNumber);
+            }
+            else if (Operator == '/')
+            {
+                             
+                Console.WriteLine("Output:\n {0} / {1} = {2}", FirstNumber, SecondNumber, FirstNumber / SecondNumber);
+            }   
+            else
+            {
+                Console.WriteLine("You enter the wrong Operation! Have Nice Day!");
+            }        
+            Console.ReadLine();
+        }
+    }
+}
